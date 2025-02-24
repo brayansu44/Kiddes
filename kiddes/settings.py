d35dab6ee1c2e4244881a245b98be131f986b07a
Kevin Marquez <jakej2747@gmail.com>
2025-02-23 21:06:27 -0500

    FICHERO DE MODULOS ACTUALIZADOS

--- kiddes/settings.py
+++ kiddes/settings.py
@@ -37,12 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bodega',
-    'cuentas_cobrar',
-    'cuentas_pagar',
-    'gastos',
-    'nomina',
-    'telas'
+    'Bodega',
+    'Cuentas',
+    'Usuarios',
+    'Compras',
+    'Nomina',
+    'Telas'
 ]
 
 MIDDLEWARE = [
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'kiddes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'KIDDES_DB',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
