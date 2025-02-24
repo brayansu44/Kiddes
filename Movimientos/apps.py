e5308361c3916e0a39c0b2e5919d2faafe81bf4b
Kevin Marquez <jakej2747@gmail.com>
2025-02-23 21:21:31 -0500

    MODULOS Y MER ACTUALIZADO, SE ESTRUCTURAN CORRECTAMENTE CONFORME A LA RELACION DE LOS MODULOS CON SU RESPECTIVA MIGRACIÓN A MYSQL

--- Movimientos/apps.py
+++ Movimientos/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CuentasCobrarConfig(AppConfig):
+class MovimientosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Cuentas'
+    name = 'Movimientos'
--- Ventas/apps.py
+++ Ventas/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CuentasCobrarConfig(AppConfig):
+class VentasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Cuentas'
+    name = 'Ventas'
