37101840855e1eca72e995f67e8d83bf04f44d64
Kevin Marquez <jakej2747@gmail.com>
2025-03-07 21:04:27 -0500

    Se genera el models de Ventas y Compras

--- Ventas/admin.py
+++ Ventas/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Ventas, Inventario_Local
 # Register your models here.
+
+#@admin.register(VentasAdmin)
+class VentasAdmin(admin.ModelAdmin):
+    list_display = ('LocalID', 'Factura', 'Fecha', 'ClienteID')
+    search_fields = ('LocalID', 'ClienteID')
+    list_filter = ()
+
+#@admin.register(Inventario_LocalAdmin)
+class Inventario_LocalAdmin(admin.ModelAdmin):
+    list_display = ('LocalID', 'ProductoID', 'StockID', 'Fecha_Actualizada', 'Qty_Minimo')
+    search_fields = ('LocalID', 'ProductoID')
+    list_filter = ()
+
+
+admin.site.register(Ventas, VentasAdmin)  
+admin.site.register(Inventario_Local, Inventario_LocalAdmin)
